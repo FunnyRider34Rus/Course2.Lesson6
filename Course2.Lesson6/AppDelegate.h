1e81567b033f6d95ce998ab881289df7667a0032
elpablo <elpablo.volgograd@yandex.ru>
2023-09-25 09:27:20 +0300

    Initial Commit

---
+++ Course2.Lesson6/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Course2.Lesson6
+//
+//  Created by Павел on 25.09.2023.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Course2.Lesson6/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  Course2.Lesson6
+//
+//  Created by Павел on 25.09.2023.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
